fe377d94602e8e5b11204d40b6f217e3b29c2c58
Jorsk <jordandaryanani@gmail.com>
2024-12-31 10:21:06 +0800

    fixed heroku deployment and added 3 movies

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,7 +18,13 @@ more_movies = [
   {:title => 'Nomadland', :rating => 'R',
     :release_date => '19-Feb-2021'},
   {:title => 'CODA', :rating => 'PG-13',
-    :release_date => '13-Aug-2021'}
+    :release_date => '13-Aug-2021'},
+  {:title => 'Detective Pikachu', :rating => 'G',
+    :release_date => '9-May-2019'},
+  {:title => 'Sonic the Hedgehog 3', :rating => 'PG',
+    :release_date => '26-Dec-2024'},
+  {:title => 'Spiderman Into the Spideverse', :rating => 'PG',
+    :release_date => '13-Dec-2018'}
 ]
 
 more_movies.each do |movie|
