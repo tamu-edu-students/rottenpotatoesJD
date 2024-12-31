7db14e4a5a539ec89d5d63797a0f7c241f14d37a
Jorsk <jordandaryanani@gmail.com>
2024-12-31 10:13:19 +0800

    test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,7 +13,7 @@ Rails.application.configure do
   config.consider_all_requests_local = false
 
   # Turn on fragment caching in view templates.
-  config.action_controller.perform_caching = true
+  #config.action_controller.perform_caching = true
 
   # Cache assets for far-future expiry since they are all digest stamped.
 
